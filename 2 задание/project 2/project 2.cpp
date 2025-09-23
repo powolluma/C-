f39c944e6948fe9949d846d806e1c8616758584f
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2025-09-23 09:55:53 +0300

    Add files via upload

---
+++ 2 задание/project 2/project 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	using namespace std;
+	
+	int a, b, c;
+
+	cout << "Введите три числа\n";
+	cin >> a >> b >> c;
+
+	float diskriminant = b ^ 2 - (4 * a * c);
+	cout << "Дискриминант = " << diskriminant;
+
+	return 0;
+}
