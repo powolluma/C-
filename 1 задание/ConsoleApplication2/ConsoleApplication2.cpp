c00f7015a555cc06d7389fff8c29dec658cd9604
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2025-09-23 09:54:36 +0300

    Add files via upload

---
+++ 1 задание/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    int a, b, c;
+    cout << "Введите три стороны треугольника:\n";
+    cin >> a >> b >> c;
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        cout << "Не треугольник";
+    }
+
+    else if (a == b && b == c) {
+        cout << "Равносторонний";
+    }
+    else if (a == b || a == c || b == c) {
+        cout << "Равнобедренный";
+    }
+    else {
+        cout << "Разносторонний";
+    }
+
+    return 0;
+}
