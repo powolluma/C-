8860bae8207236de0182e257be5b1aa8b8edeb94
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2025-11-07 17:35:35 +0900

    Add files via upload

---
+++ 17.11-1/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Degree(int a, int b){
+    int result = 1;
+    for(int i = 0; i < b; i++){
+        result *= a;
+    }
+    return result;
+}
+
+int main(){
+    setlocale(LC_ALL, "ru");
+
+    int result = Degree(5,5);
+    cout << "Результат: " << result;
+
+    return 0;
+}
